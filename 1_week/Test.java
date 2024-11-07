9956adfd605a4f8f4e4fceeb42d155189d83a27e
Tomas Salgado <tomassalgadodev@gmail.com>
2024-11-07 08:41:49 -0800

    Complete question 2 of discussion 1

---
+++ 1_week/Test.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Test {
+    public static int mystery(int[] inputArray, int k) {
+        int x = inputArray[k];
+        int answer = k;
+        int index = k + 1;
+        while (index < inputArray.length) {
+            if (inputArray[index] < x) {
+                x = inputArray[index];
+                answer = index;
+            }
+            index = index + 1;
+        }
+        return answer;
+    }
+    public static String mystery2(int[] inputArray) {
+        int index = 0;
+        while (index < inputArray.length) {
+            int targetIndex = mystery(inputArray, index);
+            int temp = inputArray[targetIndex];
+            inputArray[targetIndex] = inputArray[index];
+            inputArray[index] = temp;
+            index = index + 1;
+        }
+        return Arrays.toString(inputArray);
+    }
+
+    public static void main (String[] args) {
+        int[] inputArray = {0, 3, 3, 4, 6};
+        System.out.println(mystery2(inputArray));
+    }
+
+}
+
